8cc83c7a5a7b0a40567334ed1b4729bd6596373c
Nava Yodhan Reddy Kudumala <yodhareddy@gmail.com>
2018-05-19 16:12:12 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
-
-
 // End Point to Create Quiz and Questions
 app.get('/items',function(req,res){
   var newQuiz = require('./newQuiz');
-  //newQuiz.new1();
-});
+  newQuiz.new1();
+});//End of End Point
 //start server
 app.listen(3000);
--- newQuiz.js
+++ newQuiz.js
@@ -3,12 +3,14 @@ var request = require('request');
 //Uses module_test_db.js file to connect to the test database.
 var connection_test = require('./module_test_db');
 
-//var new1 = function(){
+var new1 = function(){
 
-//connection_test.query("select ExID,ExName from activityinfo where ExID IN (select ExID from courseactivities where CID = 677) and Written='n'", function(error,result,fields){
+connection_test.query("select ExID,ExName from behv.activityinfo where ExID IN (select ExID from behv.courseactivities where CID = 663) and Written='n' LIMIT 3", function(error,result1,fields){
+if(error) throw error;
 
+  for(var Ex in result1){
 
-//  for(var Ex in result){
+    console.log("outerloop");
 //To Create new Quiz
 var options_newQuiz = {
   url : 'https://unt.instructure.com/api/v1/courses/608/quizzes?access_token=',
@@ -18,8 +20,8 @@ var options_newQuiz = {
   },
   form: {
      "quiz":{
-         //"title": result[Ex].ExName,
-         "title": "title",
+         "title": result1[Ex].ExName+"-"+result1[Ex].ExID,
+         //"title": "title",
          "quiz_type": "assignment",
          "allowed_attempts": 1,
          "one_question_at_a_time": true,
@@ -32,7 +34,11 @@ var options_newQuiz = {
 
 request(options_newQuiz,function(err,res,body){
   if(err) throw err;
+  try{
   body = JSON.parse(body);
+}catch(e){
+  console.log("Invalid JSON Quiz");
+}
   console.log("New Quiz Created");
 
 //Functions:
@@ -111,16 +117,30 @@ request(options_newQuiz,function(err,res,body){
 
 
   //Get Data from Database
-  connection_test.query("select ExID,ItemNo,Question,Answer,MC1,MC2,MC3,MC4,MC5,ItemType from behv.items where ExID = 'Sy52sp' order by ItemNo asc", function(err,result,fields){
+  connection_test.query("select ExID,ItemNo,Question,Answer,MC1,MC2,MC3,MC4,MC5,ItemType from behv.items where ExID = '"+result1[Ex].ExID+"' order by ItemNo asc", function(err,result,fields){
     if(err) throw err;
 
-    for(var count in result){
+    //for(var count in result){
+    result.forEach(resul=>{
 
+
+
+
+
+
+      console.log("Inner Loop");
       //MMC type Questions:
-        if(result[count].ItemType=='mmc'){
+        //if(result[count].ItemType=='mmc'){
+        if(resul.ItemType=='mmc'){
           // Processing Answer through processAnswer function
-          var ans = processAnswerMMC(result[count].Answer,result[count].MC1,result[count].MC2,result[count].MC3,result[count].MC4,result[count].MC5);
+          //var ans = processAnswerMMC(result[count].Answer,result[count].MC1,result[count].MC2,result[count].MC3,result[count].MC4,result[count].MC5);
+          var ans = processAnswerMMC(resul.Answer,resul.MC1,resul.MC2,resul.MC3,resul.MC4,resul.MC5);
+          try{
           ans = JSON.parse(ans);
+        }catch(e)
+        {
+          console.log("Invalid Json MMC");
+        }
           var options_post = {
             url : 'https://unt.instructure.com/api/v1/courses/608/quizzes/'+body.id+'/questions?access_token=',
             method : 'POST',
@@ -130,9 +150,12 @@ request(options_newQuiz,function(err,res,body){
             form: {
               "question":{
                 "points_possible": 1,
-                "question_name": result[count].ItemNo,
-                "position": result[count].ItemNo+1,
-                "question_text": result[count].Question,
+                //"question_name": result[count].ItemNo,
+                "question_name": resul.ItemNo,
+                //"position": result[count].ItemNo+1,
+                "position": resul.ItemNo+1,
+                //"question_text": result[count].Question,
+                "question_text": resul.Question,
                 "question_type": "multiple_answers_question",
                 "answers": ans
                 }
@@ -141,9 +164,16 @@ request(options_newQuiz,function(err,res,body){
             }
           }//End of If mmc
           //Fill in the blanks type Questions:
-          if(result[count].ItemType=='fib'){
-            var ans = processAnswerFIB(result[count].Answer);
+          //if(result[count].ItemType=='fib'){
+          if(resul.ItemType=='fib'){
+            //var ans = processAnswerFIB(result[count].Answer);
+            var ans = processAnswerFIB(resul.Answer);
+            try{
             ans = JSON.parse(ans);
+          }catch(e)
+          {
+            console.log("Invalid Json ans FIB");
+          }
               var options_post = {
               url : 'https://unt.instructure.com/api/v1/courses/608/quizzes/'+body.id+'/questions?access_token=',
               method : 'POST',
@@ -153,8 +183,10 @@ request(options_newQuiz,function(err,res,body){
               form: {
     	          "question":{
     		          "points_possible": 1,
-    		          "question_name": result[count].ItemNo,
-    		          "question_text": result[count].Question,
+    		         // "question_name": result[count].ItemNo,
+                  "question_name": resul.ItemNo,
+    		          //"question_text": result[count].Question,
+                  "question_text": resul.Question,
     		          "question_type": "short_answer_question",
 			            "answers": ans
     	           }
@@ -163,10 +195,17 @@ request(options_newQuiz,function(err,res,body){
           }//End of If fib
 
           //tf type Questions:
-            if(result[count].ItemType=='tf'){
+            //if(result[count].ItemType=='tf'){
+            if(resul.ItemType=='tf'){
               // Processing Answer through processAnswer function
-              var ans = processAnswerTF(result[count].Answer);
+              //var ans = processAnswerTF(result[count].Answer);
+              var ans = processAnswerFIB(resul.Answer);
+              try{
               ans = JSON.parse(ans);
+            }catch(e)
+            {
+              console.log("Invalid Json ans TF");
+            }
               var options_post = {
                 url : 'https://unt.instructure.com/api/v1/courses/608/quizzes/'+body.id+'/questions?access_token=',
                 method : 'POST',
@@ -176,9 +215,12 @@ request(options_newQuiz,function(err,res,body){
                 form: {
                   "question":{
                     "points_possible": 1,
-                    "question_name": result[count].ItemNo,
-                    "position": result[count].ItemNo+1,
-                    "question_text": result[count].Question,
+                    //"question_name": result[count].ItemNo,
+                    "question_name": resul.ItemNo,
+                    //"position": result[count].ItemNo+1,
+                    "position": resul.ItemNo+1,
+                    //"question_text": result[count].Question,
+                    "question_text": resul.Question,
                     "question_type": "true_false_question",
                     "answers": ans
                     }
@@ -189,21 +231,23 @@ request(options_newQuiz,function(err,res,body){
 
 
           //Post Request to Canvas
-          if(result[count].ItemType=='mmc' || result[count].ItemType=='fib' || result[count].ItemType=='tf'){
+          //if(result[count].ItemType=='mmc' || result[count].ItemType=='fib' || result[count].ItemType=='tf'){
+          if(resul.ItemType=='mmc' || resul.ItemType=='fib' || resul.ItemType=='tf'){
             request(options_post,function(err,res,body){
               if(err) throw err;
             });
           }
 
 
-    }//Inner Items for loop ends here
+  //  }//Inner Items for loop ends here
+    });// End of Inner forEach loop
 
   });//End of items connection_query function
 
 
-});
-//}//End of outer for loop
-//});//End of courseactivities function.
-//};//End of function
-module.exports = options_newQuiz;
-//exports.new1 = new1;
+});//End of request call
+}//End of outer for loop
+});//End of courseactivities function.
+};//End of function
+//module.exports = options_newQuiz;
+exports.new1 = new1;
